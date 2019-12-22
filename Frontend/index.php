95c07ff2967d2bc40a06714878aae68b549a66e6
Luis <luishdzprgmail.com>
2019-12-22 00:39:42 -0600

    Primera version  1.1

---
+++ Frontend/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once "controller/plantilla.controlador.php";
+$plantilla = new ControladorPlantilla();
+$plantilla->plantilla();
---
+++ Frontend/view/plantilla.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="title" content="Tienda Virtual">
+    <meta name="description" content="Esto es  una  prueba">
+    <meta name="keyword" content="preuba tienda virtual">
+    <title>Tienda Virtual</title>
+    <link rel="stylesheet" href="view/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="view/css/plugins/font-awesome.min.css">
+    <link rel="stylesheet" href="view/css/plantilla.css">
+    <link rel="stylesheet" href="view/css/cabezote.css">
+    
+    <script script="view/js/plugins/jquery.min.js"></script>
+    <script script="view/js/plugins/bootstrap.min.js"></script>
+</head>
+<body>
+    <?php
+        include "modules/cabezote.php";
+    ?>
+</body>
+</html>
